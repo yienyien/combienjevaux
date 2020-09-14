c2fb749b77a1d7fd320fc91500a1c8cb50584694
Aurelien Moreau <aurelien.moreau@angus.ai>
2020-09-14 09:26:35 +0200

    Improve navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,8 @@
 <template>
 <b-container class="topcontainer">
   <b-nav tabs class="mb-2">
-    <b-nav-item><router-link to="/">Calculette</router-link></b-nav-item>
-    <b-nav-item><router-link to="/about">A propos</router-link></b-nav-item>
+    <b-nav-item :to="$router.resolve('/').href" exact exact-active-class="active">Calculette</b-nav-item>
+    <b-nav-item :to="$router.resolve('/about').href" exact exact-active-class="active">A propos</b-nav-item>
   </b-nav>
   <router-view/>
 </b-container>
